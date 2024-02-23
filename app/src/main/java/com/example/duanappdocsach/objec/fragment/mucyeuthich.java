a72108bb261dda120feb124c6abe8f8bb0bef90f
Trongnghia <trongnghiahm123@gmail.com>
2024-02-23 19:19:32 +0700

    navigation

---
+++ app/src/main/java/com/example/duanappdocsach/objec/fragment/mucyeuthich.java
@@ -0,0 +1,20 @@
+package com.example.duanappdocsach.objec.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.duanappdocsach.R;
+
+public class mucyeuthich extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.mucyeuthich_nav,container,false);
+    }
+}
