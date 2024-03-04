1d6527ef1ce6cfd56287635e2151826eacb895cd
UinLan <kimlanlongtuyen@gmail.com>
2024-03-04 09:20:37 +0700

    Thêm Login và Register

---
+++ app/src/main/java/com/example/duanappdocsach/objec/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.duanappdocsach.objec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.duanappdocsach.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
